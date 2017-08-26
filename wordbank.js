06aacfd95c24467852e8e062f90973d8901e4bbc
Tony Cuffia <westbmoretone@Tonys-MacBook-Pro.local>
2017-08-26 14:09:26 -0400

    week3hw

---
+++ wordbank.js
@@ -0,0 +1,47 @@
+Words = {};
+Words.List = [];
+
+
+
+
+
+Words.List[0] = "";
+Words.List[1] = "";
+Words.List[2] = "";
+Words.List[3] = "";
+Words.List[4] = "";
+Words.List[5] = "";
+Words.List[6] = "";
+Words.List[7] = "";
+Words.List[8] = "";
+Words.List[9] = "";
+
+
+
+Words.List[0] = "nike";
+Words.List[1] = "nba";
+Words.List[2] = "mia";
+Words.List[3] = "check";
+Words.List[4] = "love";
+Words.List[5] = "kobe";
+Words.List[6] = "jordan";
+Words.List[7] = "west";
+Words.List[8] = "okc";
+Words.List[9] = "lebron";
+
+
+
+
+
+Words.List[10] = "east";
+Words.List[11] = "portland";
+Words.List[12] = "philly";
+Words.List[13] = "blazers";
+Words.List[14] = "heat";
+Words.List[15] = "lakers";
+Words.List[16] = "wizards";
+Words.List[17] = "bos";
+Words.List[18] = "wsh";
+Words.List[19] = "losangeles";
+
+Words.Length = Words.List.length 
